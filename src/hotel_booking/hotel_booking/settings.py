a1df41d5bf47060079e3c86d4956bc4c5223fd0e
Daniil-Mb <sedferiop@gmail.com>
2026-08-20 16:53:01 +0300

    add Mypy

--- src/hotel_booking/hotel_booking/settings.py
+++ src/hotel_booking/hotel_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_97n*&e-@14u0vvtob*3v$*xkk)-(u&h212g26ne-zje+co(p+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
